b6ac3faed26b1472d75d6aec34076abfc5efabcd
Tae <118479567+TaePapich@users.noreply.github.com>
2022-11-17 23:15:00 +0300

    Изменил цены на Мед. принадлежности

--- Contents/mods/Zvezda Vendors/media/lua/client/vendorcontextmenu.lua
+++ Contents/mods/Zvezda Vendors/media/lua/client/vendorcontextmenu.lua
@@ -36,9 +36,9 @@ vendorsCafe[1] = {"Appetizers", {"SapphCooking.WhiteChocolate", 5}, {"СakeCarro
 vendorsCafe[2] = {"Drinks", {"JuiceBox", 5}}
 vendorsCafe[3] = {"ZvezdaTableWare"}
 
-vendorsMedical[1] = {"Medical", {"AlcoholWipes", 25}, {"Bandaid", 50}, {"Antibiotics", 100}, {"PillsAntiDep", 100}, {"Bandage", 10}, {"CottonBalls", 10000}, {"PillsVitamins", 10000}, {"Disinfectant", 10000}, {"Pills", 10000}};
-vendorsMedical[2] = {"ZvezdaAccessories", {"Hat_SurgicalMask_Blue", 10000}, {"Hat_SurgicalCap_Blue", 10000}, {"Gloves_Surgical", 10000}};
-vendorsMedical[3] = {"ZvezdaTools", {"Tweezers", 50}, {"Scalpel", 10000}, {"SutureNeedle", 10000}, {"SutureNeedleHolder", 10000}, {"Splint", 10000}};
+vendorsMedical[1] = {"Medical", {"AlcoholWipes", 25}, {"Bandaid", 50}, {"Antibiotics", 100}, {"PillsAntiDep", 100}, {"Bandage", 10}, {"CottonBalls", 10}, {"PillsVitamins", 25}, {"Disinfectant", 10000}, {"Pills", 10000}};
+vendorsMedical[2] = {"ZvezdaAccessories", {"Hat_SurgicalMask_Blue", 15}, {"Hat_SurgicalCap_Blue", 10}, {"Gloves_Surgical", 20}};
+vendorsMedical[3] = {"ZvezdaTools", {"Tweezers", 50}, {"Scalpel", 45}, {"SutureNeedle", 50}, {"SutureNeedleHolder", 60}, {"Splint", 100}};
 
 
 vendorsLaundry[1] = {"ZvezdaTools", {"Needle", 10000}};
